27a7d6ad63bfcae833ac8f4445b6263321294ee9
dbakti7 <dbakti1605@gmail.com>
2018-02-07 17:23:47 +0800

    Added parser from file into dictionary.

--- parser.py
+++ parser.py
@@ -6,40 +6,58 @@ import csv
 # 316,262696,42,1,2
 # 52,354280,4,1,10
 
-# {user_id: [[date, id1, id2, id3], ...]}
 id3 = 0
 user_id = 1
 id2 = 2
 date = 3
 id1 = 4
 
-data = {}
 weekLabel = "week"
 csvExtension = ".csv"
 
-# splitting the csv into 8 different files, based on the week
-fileName = weekLabel + "1" + csvExtension
-weekCount = 1
-with open('train.csv', newline='') as csvfile:
-    r = csv.reader(csvfile, delimiter=',')
-    heading = True
-    writeFile = open(fileName, 'a', newline='')
-    w = csv.writer(writeFile, delimiter=',')
-    for row in r:
-        if(heading):
-            heading = False
-            continue
-        currentWeek = ((int(row[date]) - 1) // 7) + 1
-        if(currentWeek != weekCount):
-            weekCount = currentWeek
-            fileName = weekLabel + str(weekCount) + csvExtension
-            writeFile.close()
-            writeFile = open(fileName, 'a', newline='')
-            w = csv.writer(writeFile, delimiter=',')
-        w.writerow(row)
-        # if(row[user_id] in data):
-        #     data[row[user_id]].append([row[date], row[id1], row[id2], row[id3]])
-        # else:
-        #     data[row[user_id]] = [[row[date], row[id1], row[id2], row[id3]]]
+def split_file():
+    # splitting the csv into 8 different files, based on the week
+    fileName = weekLabel + "1" + csvExtension
+    weekCount = 1
+    with open('train.csv', newline='') as csvfile:
+        r = csv.reader(csvfile, delimiter=',')
+        heading = True
+        writeFile = open(fileName, 'a', newline='')
+        w = csv.writer(writeFile, delimiter=',')
+        for row in r:
+            if(heading):
+                heading = False
+                continue
+            currentWeek = ((int(row[date]) - 1) // 7) + 1
+            if(currentWeek != weekCount):
+                weekCount = currentWeek
+                fileName = weekLabel + str(weekCount) + csvExtension
+                writeFile.close()
+                writeFile = open(fileName, 'a', newline='')
+                w = csv.writer(writeFile, delimiter=',')
+            w.writerow(row)
 
+def parseData(weekNumber):
+    # parse the file for the corresponding weekNumber 
+    # into the following dictionary format:
+    # {user_id: [[date, id1, id2, id3], ...]}
+    # return the dictionary
+
+    data = {}
+    fileName = weekLabel + str(weekNumber) + csvExtension
+    with open(fileName, newline='') as csvfile:
+        r = csv.reader(csvfile, delimiter=',')
+        heading = True
+        for row in r:
+            if(heading):
+                heading = False
+                continue
+            if(row[user_id] in data):
+                data[row[user_id]].append([row[date], row[id1], row[id2], row[id3]])
+            else:
+                data[row[user_id]] = [[row[date], row[id1], row[id2], row[id3]]]
+    return data
+
+
+data = parseData(1)
 print(len(data))
